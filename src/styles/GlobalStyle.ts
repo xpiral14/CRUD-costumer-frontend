5430568eabdb5b136d7f1fe953a426f5502f54bf
Samuel Reis <sa.reis1999@gmail.com>
2020-09-05 01:46:46 -0300

    Adicionado estilo global

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin:0;
+        font-family: 'Open Sans', sans-serif;
+        box-sizing: border-box;
+        outline: none;
+    }
+`;
+
+export default GlobalStyle;
